c9f8d51b8b4f678bfae7c88992346bc41ac0c23d
dragomirnikolov032 <dragomir.nikolov.032@gmail.com>
2026-09-05 14:34:08 +0200

    Update 02_Create_RawSales_Table.sql

--- database/02_Create_RawSales_Table.sql
+++ database/02_Create_RawSales_Table.sql
@@ -1,4 +1,30 @@
-SELECT COUNT(*) AS TotalRows
-FROM RawSales;
-SELECT TOP 10 *
-FROM RawSales;
+USE EcommerceBusinessAnalysis;
+GO
+
+CREATE TABLE RawSales (
+    RowID INT,
+    OrderID NVARCHAR(30),
+    OrderDate DATE,
+    ShipDate DATE,
+    ShipMode NVARCHAR(30),
+    CustomerID NVARCHAR(30),
+    CustomerName NVARCHAR(100),
+    Segment NVARCHAR(30),
+    PostalCode NVARCHAR(20),
+    City NVARCHAR(100),
+    State NVARCHAR(100),
+    Country NVARCHAR(100),
+    Region NVARCHAR(50),
+    Market NVARCHAR(50),
+    ProductID NVARCHAR(50),
+    Category NVARCHAR(50),
+    SubCategory NVARCHAR(50),
+    ProductName NVARCHAR(255),
+    Sales DECIMAL(18,4),
+    Quantity INT,
+    Discount DECIMAL(10,4),
+    Profit DECIMAL(18,4),
+    ShippingCost DECIMAL(18,4),
+    OrderPriority NVARCHAR(30)
+);
+GO
