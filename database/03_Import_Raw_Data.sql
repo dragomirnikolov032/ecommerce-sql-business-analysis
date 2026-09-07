5559412ac78381ff5825d164216741da957c3ec9
Dragomir Nikolov <dragomir.nikolov.032@gmail.com>
2026-09-07 20:42:24 +0200

    03_Import_Raw_Data.sql

--- database/03_Import_Raw_Data.sql
+++ database/03_Import_Raw_Data.sql
@@ -1,17 +1,23 @@
 USE EcommerceBusinessAnalysis;
 GO
 
--- IMPORTANT:
--- Replace the file path below with the location of
--- global_superstore_raw.csv on your own computer.
+-- IMPORTANT: Update the file path for your environment.
+-- Examples:
+--   Docker (project setup): '/data/global_superstore_raw.csv'
+--   Windows (native SQL):   'C:\path\to\global_superstore_raw.csv'
+--   Linux/Mac (native SQL): '/home/youruser/.../global_superstore_raw.csv'
+--   Docker (temp copy):     '/tmp/global_superstore_raw.csv'
+
+-- ROWTERMINATOR: '\n' works for Unix/Linux/Mac line endings (this CSV).
+-- If your CSV has Windows line endings (CRLF), change to: ROWTERMINATOR = '\r\n'
 
 BULK INSERT RawSales
-FROM 'C:\path\to\global_superstore_raw.csv'
+FROM '/data/global_superstore_raw.csv'
 WITH (
     FORMAT = 'CSV',
     FIRSTROW = 2,
     FIELDQUOTE = '"',
-    ROWTERMINATOR = '0x0a',
+    ROWTERMINATOR = '\n',
     TABLOCK
 );
 GO
